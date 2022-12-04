abe8ca199cfa7cd50955b7438ded69c0dfbe0ba2
Gerges-Hana <83248914+Gerges-Hana@users.noreply.github.com>
2022-12-04 18:24:33 +0200

    The 3 day

---
+++ index.js
@@ -0,0 +1,91 @@
+// #############  1  ####################
+// var link2="https://www.w3schools.com/default.asp";
+// var openlink;
+// function openFun(){
+//    openlink= window.open(link2, "_blank");
+// }
+// function closeFun(){
+//     openlink.close();
+// }
+
+// #####################   2   #########################
+
+// var StartBtn = document.getElementById("StartBtn");
+// StartBtn.addEventListener("click",showClock());
+// alert("Clock Started");
+// setInterval(showClock,1000);
+// function showClock(){
+//     d = new Date();
+//     document.getElementById('timeStr').innerHTML = d.toLocaleTimeString();
+// }
+
+// #####################   3   #########################
+
+// var btnOpen = document.getElementById('btn1');
+// var btnClose = document.getElementById('btn2');
+// btnOpen.addEventListener("click",openWindow);
+// btnClose.addEventListener("click",closeWindow)
+// let myWindow;
+// function openWindow(){
+//     myWindow = window.open("", "_blank", "width=500, height=500");
+    
+
+// }
+// function type(){
+//     var text = "gerges hana farg saad";
+//     var i;
+//     var o = "";
+//     for(i = 0;i < text.length;i++){
+//     o += text[i]
+//     document.write(o[i])
+    
+//     }
+// }
+// myWindow.setTimeout(openWindow,3000);
+
+// myWindow.setInterval(type(), 1000);
+// function closeWindow(){
+//     myWindow.close();
+// }
+
+
+// #####################   4  #########################
+// document.addEventListener("scroll",function scrolling(){
+//     if(scrollY>777)
+//     {
+//         alert('hi');
+//     }
+// })
+
+ 
+
+// ########################  5  ##########################
+// var add =document.getElementById("add");
+// document.addEventListener("click",addTab);
+// var myWindow;
+
+// function addTab(){
+//     myWindow = window.open("", "_blank", "width=500, height=500");
+//     console.dir(myWindow);
+//     myWindow.document.write("lkujoiho");
+// }
+// setTimeout(function(){
+//         myWindow.close();
+// } ,5000);
+
+// ###################  6  ####################
+
+// var add =document.getElementById("add");
+// document.addEventListener("click",addTab);
+// var myWindow;
+
+// function addTab(){
+//     myWindow = window.open("", "_blank", "width=500, height=500000");
+//     console.dir(myWindow);
+//     myWindow.document.write("lkujoiho");
+//     // myWindow.scrollbars.visible;
+//    console.dir(myWindow.document);
+  
+   
+// }
+
